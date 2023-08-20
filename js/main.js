106b710a19c0c8e3e058df4db0c6f9e72a2c5312
anri103 <ianrianri@gmail.com>
2023-08-21 01:26:01 +0300

    add page

--- js/main.js
+++ js/main.js
@@ -37,3 +37,23 @@ window.addEventListener("load", function () {
 function goBack() {
     window.history.back();
 }
+
+// Получаем ссылки на поля формы и кнопку отправки
+var registrationName = document.getElementById('registrationName');
+var registrationEmail = document.getElementById('registrationEmail');
+var registrationPass = document.getElementById('registrationPass');
+var registrationSubmitBtn = document.getElementById('registrationSubmitBtn');
+
+// Слушаем событие ввода в поля формы
+registrationName.addEventListener('input', checkForm);
+registrationEmail.addEventListener('input', checkForm);
+registrationPass.addEventListener('input', checkForm);
+
+// Функция для проверки, заполнены ли обязательные поля формы
+function checkForm() {
+    if (registrationName.checkValidity() && registrationEmail.checkValidity() && registrationPass.checkValidity()) {
+        registrationSubmitBtn.classList.remove('disabled');
+    } else {
+        registrationSubmitBtn.classList.add('disabled');
+    }
+}
